30a67323c05ce52be3a80afad5ef5d6376ef91f4
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-07-13 12:24:33 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,12 +1,13 @@
 train_folder1 = r'G:\11\BURIC_MARIO_060Y(1)\series11002-unknown'
 
 path = r'G:\11'
+fold1 = r'G:\11\img1'
 data_path1 = os.path.join(path, 'train1.hdf5')
 hdf5_file1 = h5py.File(data_path1, "w")
 
 addrs = []
 fase = 11
-slic_first = 7
+slic_first = 5
 slic_last = 15
 
 frame = (30*(slic_first-1)+fase)-1
@@ -27,7 +28,10 @@ hdf5_file1.create_dataset('mask', (len(addrs),512,512), dtype=np.uint8)
 for i in range(len(addrs)):
     dcmPath = os.path.join(train_folder1, addrs[i])
     dicom_dataset = pydicom.dcmread(dcmPath)
-    hdf5_file1['data'][i,...] = dicom_dataset.pixel_array
+    img = dicom_dataset.pixel_array
+    hdf5_file1['data'][i,...] = img
+    
+    cv2.imwrite(os.path.join(fold1, str(i) + '.png'), img)
     
     LV = cv2.inRange(img, (235, 0, 0), (255, 0, 0))
     LV = imfill(LV)
@@ -40,13 +44,13 @@ for i in range(len(addrs)):
     if i == 0:
         hdf5_file1['rows'][i,...] = dicom_dataset.Rows
         hdf5_file1['cols'][i,...] = dicom_dataset.Columns
-        hdf5_file1['x'][i,...] = 0.6641
-        hdf5_file1['y'][i,...] = 0.6641
+        hdf5_file1['x'][i,...] = 0.625
+        hdf5_file1['y'][i,...] = 0.625
         hdf5_file1['z'][i,...] = abs(dicom_dataset.SliceThickness)
         
 hdf5_file1.close()
 
-
+'''
 file1 = r'G:\11\train1.hdf5'
 fold1 = r'G:\11\img1'
 data = h5py.File(file1,'r')
@@ -56,3 +60,4 @@ for i in range(len(data['data'])):
     img = img.astype("uint8")
     cv2.imwrite(os.path.join(fold1, str(i) + '.png'), img)
 data.close()
+'''
